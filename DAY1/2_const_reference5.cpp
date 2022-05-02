808fa6fe4d8ec7f1175be027f203a47ba37fb7a6
ecourse <smkang@codenuri.co.kr>
2022-05-02 11:29:56 +0900

    Add files via upload

--- DAY1/2_const_reference5.cpp
+++ DAY1/2_const_reference5.cpp
@@ -6,7 +6,11 @@ class People
 	std::string name;
 public:
 	// 아래 코드는 C++98 시절에는 좋은 코드 입니다.
-	void setName(const std::string& s) { name = s;	}
+	// 하지만 C++11 이후에는 "move"를 지원하지 못하는 "old style code" 입니다.
+//	void setName(const std::string& s) { name = s;	}
+	void setName(const std::string& s) { name = std::move(s); }
+									// 이렇게도 move는 안됩니다.
+									// const 는 move 될수 없으므로
 };
 
 int main()
