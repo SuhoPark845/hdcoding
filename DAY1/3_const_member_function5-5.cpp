f3bb91f6f0dc491cefa4242949e70ee0a1cef46e
ecourse <smkang@codenuri.co.kr>
2022-05-02 15:29:41 +0900

    Add files via upload

--- DAY1/3_const_member_function5-5.cpp
+++ DAY1/3_const_member_function5-5.cpp
@@ -24,13 +24,27 @@ class Truck : public RefCount< Truck  >
 public:
 	~Truck() { std::cout << "~Truck" << std::endl; }
 };
+// AddRef 와 Release를 자동으로 호출하는 간단한 도구(스마트포인터)
+// 를 제공하면 편리 합니다.
+template<typename T> 
+class AutoPtr
+{
+	T* obj;
+public:
+	AutoPtr(T* p = nullptr) : obj(p) {}
+	AutoPtr(const AutoPtr& ap) : obj(ap.obj) {}
+	~AutoPtr() {}
 
+	// 포인터 처럼 보이기 위한 연산자 재정의
+	T* operator->() { return obj; }
+	T& operator*() { return *obj; }
+};
 int main()
 {
-	Truck* p1 = new Truck;
+	AutoPtr<Truck> p1 = new Truck;
 	p1->AddRef();
 
-	Truck* p2 = p1;
+	AutoPtr<Truck> p2 = p1;
 	p2->AddRef();
 
 	p1->Release();
