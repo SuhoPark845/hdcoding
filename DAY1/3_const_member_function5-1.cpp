185f7774b3f024f715faca08c8a6ab7e091c7a5c
ecourse <smkang@codenuri.co.kr>
2022-05-02 14:40:27 +0900

    Add files via upload

---
+++ DAY1/3_const_member_function5-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+class Car
+{
+	int speed;
+	int ref = 0;
+public:
+	~Car() { std::cout << "~Car" << std::endl; }
+
+	void AddRef() { ++ref; }
+	void Release() { if (--ref == 0) delete this; }
+};
+
+int main()
+{
+	Car* p1 = new Car;
+	p1->AddRef();
+	Car* p2 = p2; // 포인터 복사 시 참조 계수 증가
+	p2->AddRef();
+
+	// 포인터 사용후에는 직접 지우지 말고 참조 계수 감소
+	p2->Release();
+	p1->Release();
+}
