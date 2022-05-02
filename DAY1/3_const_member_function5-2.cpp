fce91279ae5486c83ef9d5b4be51dd11b4cf6da6
ecourse <smkang@codenuri.co.kr>
2022-05-02 14:49:46 +0900

    Add files via upload

--- DAY1/3_const_member_function5-2.cpp
+++ DAY1/3_const_member_function5-2.cpp
@@ -7,8 +7,17 @@ class RefCount
 	int ref = 0;
 public:
 	void AddRef() { ++ref; }
-	void Release() { if (--ref == 0) delete this; }
+
+	void Release()  // void Release( RefCount* this) 처럼되는것
+	{				// 즉, this는 지금 RefCount* 타입 입니다.
+		if (--ref == 0) 
+			delete this; 
+	}
+
+	// 기반 클래스 소멸자는 반드시 가상함수가 되어야 합니다.
+	virtual ~RefCount() {}
 };
+
 // Truck 도 참조 계수 기술로 관리하고 싶다.
 class Truck : public RefCount 
 {
