e801affe03745ec1e5892f5f65d6f5792672a4a2
ecourse <smkang@codenuri.co.kr>
2022-05-02 13:26:20 +0900

    Add files via upload

--- DAY1/2_const_reference9.cpp
+++ DAY1/2_const_reference9.cpp
@@ -5,6 +5,7 @@
 // setter 등을 만들때만 move를 고려하면 됩니다.
 // 단지, 함수 인자로만 사용할때는 move 고려할 필요 없습니다.
 
+
 // 아래 같은 함수는 move 지원 버전을 생각할필요는 없습니다.
 // "내부적으로 전달받은 문자열을 보관하는 것은 아니므로..."
 /*
@@ -24,4 +25,10 @@ int main()
 {
 	std::string s = "sldjfsdlkfsdfsdkfjlsd";
 	print_msg(s);
+
+
+	// 참고. string_view 개념
+	std::string      s2 = s; // s2가 s의 문자열을 복사해서 사용합니다.
+	std::string_view sv = s; // sv는 s의 문자열을 같이 사용합니다
+	sv[0] = 'A'; // error. sv 는 읽기만 됩니다
 }
