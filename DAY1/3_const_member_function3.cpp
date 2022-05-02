f70b95cfc4ab91b97fbe5ff1716ce2413a11b8d9
ecourse <smkang@codenuri.co.kr>
2022-05-02 14:16:10 +0900

    Add files via upload

--- DAY1/3_const_member_function3.cpp
+++ DAY1/3_const_member_function3.cpp
@@ -6,12 +6,15 @@ class Point
 	int x;
 	int y;
 
-	char cache[32];
-	bool cache_valid = false;
+	// mutable 멤버 데이타 : 상수 함수 안에서도 멤버 데이타를 
+	//						수정할수 있게하기 위해 사용
+	mutable char cache[32];
+	mutable bool cache_valid = false;
 public:
 	Point(int a = 0, int b = 0) : x(a), y(b) {}
 
-	const char* toString() 
+	// 아래 함수는 분명 x, y 의 값을 변경하지 않으면 "상수함수"가 되어야 합니다.
+	const char* toString() const 
 	{
 		if (cache_valid == false)
 		{
