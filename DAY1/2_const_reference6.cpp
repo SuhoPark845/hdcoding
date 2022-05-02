4ff44502d00832df877ea510d4a38ced1ec1f0c2
ecourse <smkang@codenuri.co.kr>
2022-05-02 11:42:42 +0900

    Add files via upload

---
+++ DAY1/2_const_reference6.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+class People
+{
+	std::string name;
+	std::string addr;
+public:
+	// 아래 생성자도 C++98 에서는 아주 좋은 코드 입니다.
+	// 그런데, move 를 지원하지 못하는 코드 입니다.
+	People(const std::string& name, const std::string& addr)
+		: name(name), addr(addr)
+	{
+	}
+	People(std::string&& name, std::string&& addr)
+		: name(std::move(name) ), addr(std::move(addr) )
+	{
+	}
+	People(std::string&& name, const std::string& addr)
+		: name(std::move(name)), addr(addr)
+	{
+	}
+	People(const std::string& name, std::string&& addr)
+		: name(name), addr(std::move(addr))
+	{
+	}
+	// 결론 인자가 2개인 setter(또는 생성자)를 move를 지원하게 하려면 
+	// 위 처럼 4개를 만들어야 합니다.
+	// 인자가 3개라면 "8" 개 필요
+};
+
+int main()
+{
+	std::string s1 = "john";
+	std::string s2 = "seoul";
+
+	People p1(s1, s2);
+//	People p2(std::move(s1), std::move(s2)); 
+//	People p2(std::move(s1), s2);
+	People p2(s1, std::move(s2));
+}
