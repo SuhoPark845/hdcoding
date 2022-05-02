50365046f9c73b70d4c1baa062c71fd65688716e
ecourse <smkang@codenuri.co.kr>
2022-05-02 09:40:36 +0900

    Add files via upload

---
+++ DAY1/0_temporary3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a, int b) { std::cout << "Point()" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+
+// 언제 임시객체를 사용하는가 ?
+void foo(const Point& pt) {}
+
+int main()
+{
+//	Point pt(1, 2);
+//	foo(pt);
+
+	// 핵심 1. 함수 인자로만 사용하는 객체가 필요 하다면
+	//		  임시객체 형태로 전달하는 것이 효율적이다.
+	foo( Point(1, 2) );
+
+	std::cout << "-----" << std::endl;
+}
+
+
+
+
+
