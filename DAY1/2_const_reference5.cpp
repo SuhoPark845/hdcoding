cfd16e0cf8444665a200c4bd19a1181e332a2c82
ecourse <smkang@codenuri.co.kr>
2022-05-02 11:33:48 +0900

    Add files via upload

--- DAY1/2_const_reference5.cpp
+++ DAY1/2_const_reference5.cpp
@@ -8,9 +8,15 @@ public:
 	// 아래 코드는 C++98 시절에는 좋은 코드 입니다.
 	// 하지만 C++11 이후에는 "move"를 지원하지 못하는 "old style code" 입니다.
 //	void setName(const std::string& s) { name = s;	}
-	void setName(const std::string& s) { name = std::move(s); }
+//	void setName(const std::string& s) { name = std::move(s); }
 									// 이렇게도 move는 안됩니다.
 									// const 는 move 될수 없으므로
+
+	// std::move(s2) 의 결과는 rvalue reference(&&) 로 나오게 됩니다.
+	// 따라서, 아래 처럼 오버로딩 가능합니다.
+	// 아래 코드가 C++11 에서의 최상의 setter 입니다.
+	void setName(const std::string& s) { name = s; }
+	void setName(std::string&& s)      { name = std::move(s); }
 };
 
 int main()
